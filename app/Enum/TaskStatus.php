0800fe0f7d40813bfe17d9ce03f24e60388df415
Ilshat Khabirov <khabdev@yandex.ru>
2025-04-25 11:13:24 +0300

    fix using enum

--- app/Enum/TaskStatus.php
+++ app/Enum/TaskStatus.php
@@ -7,4 +7,12 @@ enum TaskStatus: string
     case PENDING = 'pending';
     case IN_PROGRESS = 'in_progress';
     case COMPLETED = 'completed';
+
+    public static function values(): array {
+        return [
+            self::PENDING,
+            self::IN_PROGRESS,
+            self::COMPLETED,
+        ];
+    }
 }
