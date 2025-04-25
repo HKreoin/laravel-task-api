0af5ad4f908885dfc66bff4b82e93eb2f0b0916f
Ilshat Khabirov <khabdev@yandex.ru>
2025-04-25 11:36:35 +0300

    update Task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,31 +5,43 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
+use Illuminate\Http\JsonResponse;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json([
+            'success' => true,
+            'data' => Task::all()
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreTaskRequest $request)
+    public function store(StoreTaskRequest $request): JsonResponse
     {
-        //
+        $task = Task::create($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Task created successfully',
+            'data' => $task
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Task $task)
+    public function show(Task $task): JsonResponse
     {
-        //
+        return response()->json([
+            'success' => true,
+            'data' => $task
+        ]);
     }
 
     /**
@@ -37,7 +49,12 @@ class TaskController extends Controller
      */
     public function update(UpdateTaskRequest $request, Task $task)
     {
-        //
+        $task->update($request->validated());
+        return response()->json([
+            'success' => true,
+            'message' => 'Task updated successfully',
+            'data' => $task
+        ]);
     }
 
     /**
@@ -45,6 +62,10 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Task deleted successfully',
+        ]);
     }
 }
