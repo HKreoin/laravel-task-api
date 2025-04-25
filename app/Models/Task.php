1dbd7caca33831d867e4d2d3acf2904cf04f5ea4
Ilshat Khabirov <khabdev@yandex.ru>
2025-04-25 10:58:14 +0300

    migration for Task, enum for Task statuses, casts status in Task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,13 +2,19 @@
 
 namespace App\Models;
 
+use App\Enum\TaskStatus;
+use Database\Factories\TaskFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    /** @use HasFactory<TaskFactory> */
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'status'];
+
+    protected $casts = [
+        'status' => TaskStatus::class
+    ];
 }
