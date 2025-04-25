7f5c8ab82d0cd5771c07f8af961fefab7e4869b3
Ilshat Khabirov <khabdev@yandex.ru>
2025-04-25 11:36:18 +0300

    configure database seeders

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,10 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Enum\TaskStatus;
+use App\Models\Task;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ * @extends Factory<Task>
  */
 class TaskFactory extends Factory
 {
@@ -17,7 +19,9 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'status' => $this->faker->randomElement(TaskStatus::cases())->value,
         ];
     }
 }
